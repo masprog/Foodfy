2d546e0340e3220dc826063b1d9f524eb2c10401
masprog <maurolove09@gmail.com>
2020-07-01 01:02:14 +0100

    creating page modal

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards){
+  card.addEventListener("click", function(){
+     modalOverlay.classList.add('active')
+  })
+}
